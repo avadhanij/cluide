0fa24a44f4cc48e1b4520d06be4248175cd7be26
Avadhani Jonnavithula <avadhanij@users.noreply.github.com>
2025-06-14 23:39:08 -0700

    feat(ci): Add automated release capability (#1)

--- cmd/version.go
+++ cmd/version.go
@@ -2,17 +2,28 @@ package cmd
 
 import (
 	"fmt"
+	"runtime/debug"
 
 	"github.com/spf13/cobra"
 )
 
-var versionString string
+// var versionString string
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of cluide",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("cluide %s\n", versionString)
+		buildInfo, ok := debug.ReadBuildInfo()
+		if !ok {
+			fmt.Println("Unable to determine version information.")
+			return
+		}
+
+		if buildInfo.Main.Version != "" {
+			fmt.Printf("Version: %s\n", buildInfo.Main.Version)
+		} else {
+			fmt.Println("Version: unknown")
+		}
 	},
 }
 
